8a897a94c7a717d4f4761c9be28fd23bf344f9b3
Rouddy <rouddy@naver.com>
2025-06-24 15:57:44 +0900

    swift package manager 에서 wifi provisioner dependency가 안된 것 수정

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AlgorigoBleLibrary",
     platforms: [
-        .macOS(.v10_13), .iOS(.v11)
+        .macOS(.v10_13), .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -19,13 +19,18 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.5.0"),
+        .package(url: "https://github.com/NordicSemiconductor/IOS-nRF-Wi-Fi-Provisioner.git", .upToNextMajor(from: "2.1.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AlgorigoBleLibrary",
-            dependencies: ["RxSwift", .product(name: "RxRelay", package: "RxSwift")],
+            dependencies: [
+                "RxSwift",
+                .product(name: "RxRelay", package: "RxSwift"),
+                .product(name: "NordicWiFiProvisioner-BLE", package: "IOS-nRF-Wi-Fi-Provisioner"),
+            ],
             path: "AlgorigoBleLibrary/AlgorigoBleLibrary"
         ),
         .testTarget(
